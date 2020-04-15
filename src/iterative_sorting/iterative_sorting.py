e8a17c450727f4bf55a75fabefb5dd6b1ccc129a
Shane Jeremich <shane.jeremich@gmail.com>
2020-04-15 13:34:29 -0600

    Both functions done

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,30 +1,39 @@
-# TO-DO: Complete the selection_sort() function below 
-def selection_sort( arr ):
-    # loop through n-1 elements
-    for i in range(0, len(arr) - 1):
-        cur_index = i
-        smallest_index = cur_index
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
-             
-
-
-
-        # TO-DO: swap
-
-
-
+# TO-DO: Complete the selection_sort() function below
+def insertion_sort(arr):
+    # current index is greater than the the next index
+    # If true then swap else selet next index to the right
+    for i in range(len(arr) - 1):
+        for j in range(i, len(arr)):
+            if arr[i] > arr[j]:
+                arr[j], arr[i] = arr[i], arr[j]
 
     return arr
 
+
+print("sort", insertion_sort([1, 2, 3, 4, 2, 6, 6, 9, 4, 1]))
 
 # TO-DO:  implement the Bubble Sort function below
-def bubble_sort( arr ):
+# bubble up the largest number to the end of array
+# start at the first and second index
+# is the first index larger than the second?
+#   If yes than swap else if NO then
+# is the second larger than the third? NO
+
+
+def bubble_sort(arr):
+    is_swapped = True
+    while is_swapped:
+        is_swapped = False
+        for i in range(len(arr) - 1):
+            if arr[i] > arr[i+1]:
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+                is_swapped = True
 
     return arr
 
 
+print("sort", bubble_sort([1, 2, 3, 4, 2, 6, 6, 9, 4, 1]))
 # STRETCH: implement the Count Sort function below
-def count_sort( arr, maximum=-1 ):
+# def count_sort(arr, maximum=-1):
 
-    return arr
+#     return arr
